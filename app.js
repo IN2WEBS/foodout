e170166a32b697c905eb81cebff78df7910ea3e6
Karolis Kerpys <karolis.kerpys@gmail.com>
2018-05-22 20:19:31 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 9000;
+
+app.use(express.static(__dirname+'/public'));
+
+app.get('/', (req,res)=>{
+   res.send('🚧 project is under construction 🚧')
+});
+app.get('/api/welcome', (req,res)=>{
+    res.json({
+      message:'Welcome!',
+      url:'/heroimage.png'
+    })
+});
+
+
+app.listen(port,()=>{
+   console.log(`server is running on port ${port}`);
+});
